f4c011035900da1a89f269c63565f0b6f47cead3
VasichkinMikhail <budarain@mail.ru>
2022-01-09 03:38:13 +0300

    Home Work 3

--- lesson_3_spring_mvc/src/main/java/ru/geekbrains/persist/Product.java
+++ lesson_3_spring_mvc/src/main/java/ru/geekbrains/persist/Product.java
@@ -15,9 +15,11 @@ public class Product {
         public Product() {
         }
 
-        public Product(Long id, String name) {
+        public Product(Long id, String name, BigDecimal price, String description) {
             this.id = id;
             this.name = name;
+            this.price = price;
+            this.description = description;
         }
 
         public Long getId() {
@@ -35,6 +37,22 @@ public class Product {
         public void setName(String name) {
             this.name = name;
         }
+
+    public String getDescription() {
+        return description;
     }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
 
 
--- lesson_3_spring_mvc/src/main/java/ru/geekbrains/persist/ProductRepositoryImpl.java
+++ lesson_3_spring_mvc/src/main/java/ru/geekbrains/persist/ProductRepositoryImpl.java
@@ -3,6 +3,7 @@ package ru.geekbrains.persist;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -17,11 +18,13 @@ public class ProductRepositoryImpl implements ProductRepository {
 
     private final AtomicLong identity = new AtomicLong(0);
 
+
+
     @PostConstruct
     public void init() {
-        this.save(new Product(1L, "Product 1"));
-        this.save(new Product(2L, "Product 2"));
-        this.save(new Product(3L, "Product 3"));
+        this.save(new Product(1L, "Product 1",new BigDecimal(160),"bla bla bla"));
+        this.save(new Product(2L, "Product 2",new BigDecimal(320),"bla bla bla"));
+        this.save(new Product(3L, "Product 3",new BigDecimal(655),"bla bla bla"));
     }
 
     @Override
@@ -37,15 +40,27 @@ public class ProductRepositoryImpl implements ProductRepository {
     @Override
     public void save(Product product) {
         if (product.getId() == null) {
-            long id = identity.incrementAndGet();
+            long id = lastId()+1;
             product.setId(id);
         }
         productMap.put(product.getId(), product);
+    }
+
+    public Long lastId() {
+        ArrayList<Product> products = new ArrayList<>(productMap.values());
+        long lastId = 0;
+        for (Product product : products) {
+            if (product.getId() > lastId) {
+                lastId = product.getId();
+            }
+        }
+        return lastId;
     }
 
     @Override
     public void delete(long id) {
         productMap.remove(id);
     }
+
 }
 
